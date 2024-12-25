45bc596488457a1d7b1427bf945fd1b0204738cf
John Ferguson <johncferguson90@gmail.com>
2024-12-25 15:25:52 -0800

    update nginx and docker-entrypoint

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Set default values for environment variables
+: "${API_HOST:=backend}"
+: "${API_PORT:=8090}"
+: "${API_PROTOCOL:=https}"
+
 # Replace environment variables in nginx.conf
 envsubst '${API_HOST} ${API_PORT} ${API_PROTOCOL}' < /etc/nginx/nginx.conf > /tmp/nginx-temp/nginx.conf.tmp
 cp /tmp/nginx-temp/nginx.conf.tmp /etc/nginx/nginx.conf
